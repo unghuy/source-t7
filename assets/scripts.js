eda3935c9c40c731f9e4431e2c771d5adb3e51c5
Ung Huy <ungquanghuy@gmail.com>
2023-08-23 20:27:22 +0700

    update style version 2.0.0

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,10 +2,10 @@ function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
 
-$('.flow-content .item').hover(
-  function(){ $('.footer').addClass('hide') },
-  function(){ $('.footer').removeClass('hide') }
-)
+// $('.flow-content .item').hover(
+//   function(){ $('.footer').addClass('hide') },
+//   function(){ $('.footer').removeClass('hide') }
+// )
 
 // Slick Slider
 $(document).ready(function(){
